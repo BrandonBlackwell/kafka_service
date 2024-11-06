9ad71e7cbf06becff7176e61b562e1ea6551e143
Brandon Blackwell <brandonb13195@gmail.com>
2024-11-05 19:05:15 -0800

    Generated all US city ids.

--- CityIds.java
+++ CityIds.java
@@ -1,9 +1,14 @@
 import org.json.JSONArray;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
+import static java.lang.String.valueOf;
+
 /*TODO:
    1. Read cityIds file into json array.
    2. Access objects containing US and store its city id
@@ -22,8 +27,21 @@ public class CityIds {
         JSONArray jsonArray = new JSONArray(content);
         CityIdParser cityIdParser = new CityIdParser(jsonArray);
         ArrayList<Integer> ids = cityIdParser.getIds();
+//        Create file to store US city IDs in
+        File file = new File("cityIds.txt");
+        FileWriter fw;
+        try {
+            fw = new FileWriter(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+//        Write ids to file
         for (int id : ids) {
-            System.out.println(id);
+            try {
+                fw.write(String.format("%d\n", id));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
